7418a3e3a209602559be33c335376fc9fd4fd803
Yuqi Li <a00974769@se14l0124-25.ad.bcit.ca>
2018-02-20 17:05:38 -0800

    stickers sticks

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var io = require("socket.io")(server);
 //var allusers2 = [];
 
 var allusers = {};
+var allstickers = {};
 
 io.on("connection", function(socket){
     console.log("connect");
@@ -17,6 +18,11 @@ io.on("connection", function(socket){
     //socket.emit("yourid", socket.id);
     
     //io.emit("userjoined", allusers);
+    
+    socket.on("stick", function(data){
+        allstickers[this.myRoom].push(data);
+        io.to(this.myRoom).emit("newsticker", allstickers[this.myRoom]);
+    });
     
     socket.on("joinroom", function(data){
         console.log(data);
@@ -29,8 +35,13 @@ io.on("connection", function(socket){
             allusers[data] = [];
         }
         
+        if(!allstickers[data]){
+            allstickers[data] = [];
+        }
+        
         allusers[data].push(socket.id);
         io.to(data).emit("userjoined", allusers[data]);
+        io.to(data).emit("newsticker", allstickers[data]);
     });
     
     socket.on("mymove", function(data){
@@ -41,10 +52,13 @@ io.on("connection", function(socket){
         /*var index = allusers.indexOf(socket.id);
         allusers.splice(index, 1);
         io.emit("userjoined", allusers);*/
-        console.log("disconnect");
-        var index = allusers[this.myRoom].indexOf(socket.id);
-        allusers[this.myRoom].splice(index,1);
-        io.to(this.myRoom).emit("userjoined", allusers[this.myRoom]);
+        if(this.myRoom){
+            console.log("disconnect");
+            var index = allusers[this.myRoom].indexOf(socket.id);
+            allusers[this.myRoom].splice(index,1);
+            io.to(this.myRoom).emit("userjoined", allusers[this.myRoom]);
+        }
+        
     })
 })
 
